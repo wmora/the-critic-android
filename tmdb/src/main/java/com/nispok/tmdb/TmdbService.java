8c32879f82b12c5b8be656bf2e1b7a199bc34615
William Mora <william.r.mora@gmail.com>
2016-08-29 21:16:02 -0400

    add configuration service

--- tmdb/src/main/java/com/nispok/tmdb/TmdbService.java
+++ tmdb/src/main/java/com/nispok/tmdb/TmdbService.java
@@ -3,6 +3,7 @@ package com.nispok.tmdb;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import rx.Observable;
+import rx.Single;
 
 public interface TmdbService {
 
@@ -19,4 +20,23 @@ public interface TmdbService {
                                               @Query("page") int page,
                                               @Query("language") String language);
 
+    /**
+     * Get the system wide configuration information. Some elements of the API require some
+     * knowledge of this configuration data. The purpose of this is to try and keep the actual API
+     * responses as light as possible. It is recommended you cache this data within your
+     * application and check for updates every few days.
+     * <p/>
+     * This method currently holds the data relevant to building image URLs as well as the change
+     * key map.
+     * <p/>
+     * To build an image URL, you will need 3 pieces of data. The base_url, size and file_path.
+     * Simply combine them all and you will have a fully qualified URL. Here’s an example URL:
+     * <p/>
+     * http://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+     *
+     * @param apiKey API key.
+     * @return system wide configuration information.
+     */
+    @GET("configuration")
+    Single<Configuration> configuration(@Query("api_key") String apiKey);
 }
